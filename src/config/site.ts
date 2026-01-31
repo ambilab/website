12268c43654c7b0fe9a219412f5cbf0ecc016a48
Vaclav Vancura <commit@vancura.dev>
2026-01-31 20:29:32 +0100

    refactor(social-links): remove outdated social media links
    Signed-off-by: Vaclav Vancura <commit@vancura.dev>

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,12 +6,6 @@ export const SITE = {
     DEFAULT_OG_IMAGE: 'https://ambilab.com/og-default.png',
 
     SOCIAL: {
-        X: 'https://x.com/ambilab',
-        THREADS: 'https://threads.net/@ambilab_games',
-        INSTAGRAM: 'https://instagram.com/ambilab_games',
-        LINKEDIN: 'https://linkedin.com/company/ambilab',
-        MASTODON: 'https://mastodon.gamedev.place/@ambilab',
-        BLUESKY: 'https://bsky.app/profile/ambilab',
         GITHUB: 'https://github.com/ambilab',
     },
 } as const;
